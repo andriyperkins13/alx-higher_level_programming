c1f5ea58e47c83e4bb56e1f50261a00b9cb080de
Olajuwon Mubarak <olajuwonmubarak@hotmail.com>
2022-10-18 20:09:50 +0100

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+""" Locked class example """
+
+
 class LockedClass:
-    """A locked class that only lets the user dynamically create the instance
-    attribute 'first_name'"""
+    """ Locked class """
     __slots__ = ['first_name']
+
+    def __init__(self):
+        """ Instance intallization """
+        pass
