2c3699552ade42c3bbda4c8138587be810822ad2
Olajuwon Mubarak <olajuwonmubarak@hotmail.com>
2022-10-25 19:37:40 +0100

    2-read_lines.py

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""
-Contains the "read_lines" function
-"""
+""" Append to a file module """
 
 
-def read_lines(filename="", nb_lines=0):
-    """reads n lines of a text file (UTF8) and prints it to stdout"""
-    with open(filename, 'r', encoding='utf-8') as f:
-        if nb_lines <= 0:
-            print(f.read(), end='')
-            return
-        i = 0
-        for i, line in enumerate(f):
-            if i == nb_lines:
-                break
-            print(line, end='')
+def append_write(filename="", text=""):
+    """ Appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
+    """
+    with open(filename, mode='a', encoding="utf-8") as files:
+        files.write(text)
+    return len(text)
