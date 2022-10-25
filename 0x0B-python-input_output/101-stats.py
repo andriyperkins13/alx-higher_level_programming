12db64886296fd9f603ef3172625a6c055218b6a
Olajuwon Mubarak <olajuwonmubarak@hotmail.com>
2022-10-25 20:42:36 +0100

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,57 +1,50 @@
 #!/usr/bin/python3
-""" Log parsing module """
-import sys
+"""Reads from standard input and computes metrics
+"""
 
 
-class LogParsing:
-    """ Reads stdin line by line and computes metrics """
-    def __init__(self):
-        """ Initalization method """
-        self.dic = {}
-        self.size = 0
-
-    def create_dic(self):
-        """ Initialization of dict """
-        self.dic['200'] = 0
-        self.dic['301'] = 0
-        self.dic['400'] = 0
-        self.dic['401'] = 0
-        self.dic['403'] = 0
-        self.dic['404'] = 0
-        self.dic['405'] = 0
-        self.dic['500'] = 0
-
-    def create_status_code(self, status):
-        """ Creates repeated number to the status code """
-        if status in self.dic:
-            self.dic[status] += 1
-
-    def display(self, sig=0, frame=0):
-        """ Print status code """
-        print("File size: {:d}".format(self.size))
-        for key in sorted(self.dic.keys()):
-            if self.dic[key] != 0:
-                print("{}: {:d}".format(key, self.dic[key]))
+def print_stats(size, status_codes):
+    """Print accumulated metrics
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 
 if __name__ == "__main__":
-    log = LogParsing()
-    log.create_dic()
-    nlines = 0
+    import sys
+
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
     try:
         for line in sys.stdin:
-            if nlines % 10 == 0 and nlines != 0:
-                log.display()
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
 
             try:
-                list_line = [x for x in line.split(" ") if x.strip()]
-                log.create_status_code(list_line[-2])
-                log.size += int(list_line[-1].strip("\n"))
-            except Exception:
+                size += int(line[-1])
+            except (IndexError, ValueError):
                 pass
-            nlines += 1
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
     except KeyboardInterrupt:
-        log.display()
+        print_stats(size, status_codes)
         raise
-    log.display()
